d79b4b230684f63a1228dc9cb341567cbea6e9bf
hyper_boar <hyperboar@gmail.com>
2026-04-15 14:43:28 +0300

    feat: alt+1 grayscale is now uses 'standard sRGB weights', the result is the same as if desaturated photoshop overlay is applied

--- content.js
+++ content.js
@@ -26,8 +26,26 @@ function toggleGreyscale(idx) {
     } 
 }
 
+function addSvgFiltersToThePage() {
+    if (document.querySelector('svg#shader-matrices')) return
+
+    const b = document.querySelector('body')
+    b.insertAdjacentHTML(
+        'beforeend',
+        `<svg xmlns="http://www.w3.org/2000/svg" style="position:absolute;width:0;height:0" id="shader-matrices">
+        <filter id="grayscale-std" color-interpolation-filters="linearRGB">
+            <feColorMatrix type="matrix" values="
+                0.2126 0.7152 0.0722 0 0
+                0.2126 0.7152 0.0722 0 0
+                0.2126 0.7152 0.0722 0 0
+                0      0      0      1 0"></feColorMatrix>
+        </filter>`
+    )
+}
+
 function initialize() {
     window.addEventListener('keydown', evt => { 
+        addSvgFiltersToThePage()
         if (evt.altKey) {
             switch(evt.code) {
                 case 'Digit1':
